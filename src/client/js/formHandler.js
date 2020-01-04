33b8b914fdc468d349075f6c2fb17d51c1cd122a
ZihanTian <zti@umich.edu>
2020-01-03 21:44:33 -0500

    can not post

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,20 +1,44 @@
 function handleSubmit(event) {
     event.preventDefault()
+    const postData = async ( url = '', data = {})=>{
+        console.log(data,'here')
+          const res = await fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data), // body data type must match "Content-Type" header        
+        });
+          console.log(res.body,'should finish the transform')
+          try {
+            const newData = await res.json();
+            console.log(newData,'new try hello');
+            return newData
+          }catch(error) {
+          console.log("error", error);
+          // appropriately handle the error
+          }
+      }
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    //Client.checkForName(formText)
+    const formText ={url:document.getElementById('name').value}; 
+    document.getElementById('isurl').innerHTML = Client.checkForName(formText.url);
     
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    //console.log("::: Form Submitted :::")
+    //fetch('http://localhost:8081/test')
+    
+    postData('http://localhost:8081/test',formText)
     //console.log(formText)
     //Client.checkForName(formText)
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    //.then(res => res.json())
+    //.then(function(res) {
+     //   document.getElementById('results').innerHTML = res.message1
+    //})
     
     
 }
 
 export { handleSubmit }
+
+
--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,26 @@
 
 const dotenv = require('dotenv');
+const cors = require('cors')
 dotenv.config();
 //console.log(`Your API key is ${process.env.API_KEY}`);
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+//const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
-
+//const bodyParser = require('body-parser')
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.use(express.static('dist'))
-
+app.use(cors())
 console.log(__dirname)
 var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id:process.env.API_ID, 
   application_key: process.env.API_KEY
 });
-var inputurl = 'http://dbpedia.org/resource/Donald_Trump';
-textapi.sentiment({
-    url:inputurl
-},
-(err,resp)=>{
-    if (err ===null){
-        console.log(resp);
-        //res.json({
-         //   message1:res.polarity,
-          //  message2: res.polarity_confidence
-        //});
-    }else{
-        //res.json({
-         //   message: failText
-        //});
-    }
-}
-)
+var inputurl 
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
@@ -44,9 +31,33 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.send(mockAPIResponse)
+app.post('/test', function (req, res) {
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8082');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    inputurl = req;
+    console.log(inputurl,'this is my url')
+    
+    //res.send(mockAPIResponse);
 })
 
 
+
+//textapi.sentiment({
+ //   url: 'http://techcrunch.com/2015/07/16/microsoft-will-never-give-up-on-mobile',
+//},
+
+//(err,resp)=>{
+    
+//    if (err ===null){
+        //console.log(resp);
+        //res.json({
+         //   message1:resp.polarity,
+          //  message2: resp.polarity_confidence
+        //});
+ //   }else{
+       // res.json({
+        //   message: 'failText'
+       // });
+//    }
+//}
+//)
